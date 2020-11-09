8897c9dacfb9127c0f8a448770b4481ba6bef356
Ziyad Zulfikar <56788667+ziyadzulfikar@users.noreply.github.com>
2020-11-09 23:44:12 +0530

    admin panel database created

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,5 +1,5 @@
 var db = require('../config/connection')
-
+var collection = require('../config/collections')
 module.exports = {
     
     addProduct:(product,callback)=>{
@@ -8,6 +8,12 @@ module.exports = {
         db.get().collection('product').insertOne(product).then((data)=>{
             console.log(data);
             callback(data.ops[0]._id)
+        })
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products = await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
         })
     }
 }
