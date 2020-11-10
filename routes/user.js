7c88396d9a864f256fbef4a5f36060959a3e9740
Ziyad Zulfikar <56788667+ziyadzulfikar@users.noreply.github.com>
2020-11-10 16:16:30 +0530

    bug fix on sign-in in user-interface

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,62 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
-var productHelper = require('../helpers/product-helpers')
+var productHelper = require('../helpers/product-helpers');
+const userHelpers = require('../helpers/user-helpers');
+
+const verifyLogin = (req,res,next)=>{
+  if(req.session.loggedIn){
+    next()
+  }else{
+    res.redirect('/login')
+  }
+}
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let user = req.session.user
+  console.log(user);
   productHelper.getAllProducts().then((products)=>{
     console.log(products);
-    res.render('user/view-products', { title: 'Company', products, admin: false});
+    res.render('user/view-products', { title: 'Company', products, user, admin: false});
   })
 });
+router.get('/login',(req,res)=>{
+  if(req.session.loggedIn){
+    res.redirect('/')
+  }else{
+    res.render('user/login', { "loginErr":req.session.loginErr , title: 'Company', admin: false})
+    req.session.loginErr = false
+  }
+})
+router.get('/signup',(req,res)=>{
+  res.render('user/signup', { title: 'Company', admin: false})
+})
+router.post('/signup',(req,res)=>{
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response);
+  })
+})
+router.post('/login',(req,res)=>{
+   userHelpers.doLogin(req.body).then((response)=>{
+     if (response.status) {
+       req.session.loggedIn = true
+       req.session.user = response.user
+       res.redirect('/')
+     }else{
+       req.session.loginErr = "Invalid Username or Password"
+       res.redirect('/login')
+     }
+   })
+})
+router.get('/logout',(req,res)=>{
+  req.session.destroy()
+  res.redirect('/')
+})
+router.get('/cart', verifyLogin,(req,res)=>{
+  res.render('user/cart', { title: 'Company', admin: false})
+  
+})
 
 module.exports = router;
